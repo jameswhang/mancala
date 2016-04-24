c05c6e6ca81df6f89e61f1e038221cf1141c573e
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2016-04-24 00:07:27 -0500

    Final version

--- Player.py
+++ Player.py
@@ -12,6 +12,8 @@ from random import *
 from decimal import *
 from copy import *
 from MancalaBoard import *
+from math import log, sqrt
+import time
 
 # a constant
 INFINITY = 1.0e400
@@ -23,7 +25,7 @@ class Player:
     MINIMAX = 2
     ABPRUNE = 3
     CUSTOM = 4
-    
+
     def __init__(self, playerNum, playerType, ply=0):
         """Initialize a Player with a playerNum (1 or 2), playerType (one of
         the constants such as HUMAN), and a ply (default is 0)."""
@@ -35,7 +37,7 @@ class Player:
     def __repr__(self):
         """Returns a string representation of the Player."""
         return str(self.num)
-        
+
     def minimaxMove(self, board, ply):
         """ Choose the best minimax move.  Returns (score, move) """
         move = -1
@@ -82,7 +84,7 @@ class Player:
             if s > score:
                 score = s
         return score
-    
+
     def minValue(self, board, ply, turn):
         """ Find the minimax value for the next move for this player
             at a given board configuation. Returns score."""
@@ -179,7 +181,7 @@ class Player:
             return self.score(board)
         score = INFINITY
         for action in board.legalMoves(self.opponent): # Examine all feasible actions by the opponent
-# make a new board
+            # make a new board
             nb = deepcopy(board)
             nb.makeMove(self.opponent, action)
             score = min(score, self.alphaBetaMaxMove(nb, alpha, beta, ply-1))
@@ -187,7 +189,74 @@ class Player:
                 return score
             beta = min(beta, score)
         return score
-                
+
+    def customMove(self, board, ply=50):
+        if self.startMove:
+            self.startMove = False
+            return INFINITY, 3
+        """ Choose a move with alpha beta pruning.  Returns (score, move) """
+        # enemy player
+        self.opponent = Player(self.opp, self.type, self.ply)
+        # Check terminal conditions
+        if board.gameOver(): # Game done
+            return self.score(board), -1
+        elif ply == 0:
+            return self.score(board), board.legalMoves(self)[0] # give up, whatever is the first one
+        alpha = -INFINITY
+        beta = -INFINITY
+        score = -INFINITY
+        move = -1
+
+        for action in board.legalMoves(self):
+            # make a new board
+            nb = deepcopy(board)
+            if nb.makeMove(self, action): # Another move can be made 
+                action_score = self.customMaxMove(nb, alpha, beta, ply-1)
+            else:
+                action_score = self.customMinMove(nb, alpha, beta, ply-1)
+            if action_score > score:
+                move = action
+                score = action_score
+            alpha = max(alpha, score)
+        return (score, move)
+
+    def customMaxMove(self, board, alpha, beta, ply):
+        """ my custom movement is modified AB Pruning.
+            It takes into account the replay move when there is another move that can be made"""
+        # Check terminal condition
+        if board.gameOver() or ply is 0:
+            return self.score(board)
+        max_score = -INFINITY
+        for action in board.legalMoves(self): # examine all feasible actions
+            # make a new board
+            nb = deepcopy(board)
+            if nb.makeMove(self, action): # Another move
+                max_score = max(max_score, self.customMaxMove(nb, alpha, beta, ply-1))
+            else: # opponent's move
+                max_score = max(max_score, self.customMinMove(nb, alpha, beta, ply-1))
+            if (max_score >= beta): # if our score is geq beta, return this score
+                return max_score
+            alpha = max(alpha, max_score) # update alpha
+        return max_score
+
+    def customMinMove(self, board, alpha, beta, ply):
+        """ Find the minimax value for the opponent """
+        """ Same thing as ABPruning Max but adds one more line to take replay into account"""
+        if board.gameOver() or ply is 0:
+            return self.score(board)
+        score = INFINITY
+        for action in board.legalMoves(self.opponent): # Examine all feasible actions by the opponent
+            # make a new board
+            nb = deepcopy(board)
+            if nb.makeMove(self.opponent, action): # Opponent makes another move
+                score = min(score, self.customMinMove(nb, alpha, beta, ply-1))
+            else:
+                score = min(score, self.customMaxMove(nb, alpha, beta, ply-1))
+            if (score <= alpha):
+                return score
+            beta = min(beta, score)
+        return score
+
     def chooseMove(self, board):
         """ Returns the next move that this player wants to make """
         if self.type == self.HUMAN:
@@ -209,7 +278,7 @@ class Player:
             print "chose move", move, " with value", val
             return move
         elif self.type == self.CUSTOM:
-            val, move = self.MTCSMove(board, self.ply)
+            val, move = self.customMove(board, 10)
             print "choose move", move, "with value", val
             return move
         else:
@@ -218,34 +287,49 @@ class Player:
 
 
 # Note, you should change the name of this player to be your netid
-class MancalaPlayer(Player):
+class syw973(Player):
     """ Defines a player that knows how to evaluate a Mancala gameboard
         intelligently """
 
-    def __init__(self, playerNum, playerType, ply = 0):
-        Player.__init__(self, playerNum, playerType, ply) 
-
+    def __init__(self, playerNum, playerType, scoretype):
+        self.ply = 100
+        Player.__init__(self, playerNum, playerType, self.ply) 
+        self.startMove = True
         self.p1score = 0 # player 1 score
         self.p2score = 0 # player 2 score
+        self.scoretype = scoretype
 
     def score(self, board):
         """ Evaluate the Mancala board for this player """
-        # Currently this function just calls Player's score
-        # function.  You should replace the line below with your own code
-        # for evaluating the board
-
         # first add what's in each player's mancala
-        self.p1score += board.scoreCups[0] 
-        self.p2score += board.scoreCups[1]
+        self.p1score = 0
+        self.p2score = 0
+
+        self.p1score += board.scoreCups[0] * 5
+        self.p2score += board.scoreCups[1] * 5
 
         # evaluate the cups on my side
-        self.p1score += sum(board.getPlayersCups(1))
-        self.p1score += sum(board.getPlayersCups(2))
+        for i in range(len(board.P1Cups)):
+            # potential for an extra move, additional weight
+            if i == board.P1Cups[i]:
+                self.p1score += board.P1Cups[i] * 1.3
+            else:
+                self.p1score += board.P1Cups[i]
 
-        # TODO: update the weights of each cup
+        for i in range(len(board.P2Cups)):
+            # potential for an extra move, additional weight
+            if i == board.P2Cups[i]:
+                self.p2score += board.P2Cups[i] * 1.5
+            else:
+                self.p2score += board.P2Cups[i]
+
+        # Compensate for winning scenarios
+        if board.scoreCups[0] > 24:
+            self.p1score *= 1.5
+        if board.scoreCups[1] > 24:
+            self.p2score *= 1.5
 
         if self.num == 1:
-            return self.p1score
+            return ( self.p1score - self.p2score ) / 48
         else:
-            return self.p2score
-
+            return ( self.p2score - self.p1score) / 48
